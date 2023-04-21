68d04357cdf38faafe82dff8c4a2588ae4d67a20
Anton Safranovski <kittowned@gmail.com>
2023-04-21 08:38:08 +0300

    Basic paint functionality, resize pen

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function init() {
     const ctx = canvas.getContext('2d');
     const clearBtn = document.querySelector('.clearBtn');
     let canDraw = false;
+    let lineWidth = 1.0;
     window.addEventListener('resize', resizeCanvas, false);
     function resizeCanvas() {
         const image = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -11,13 +12,23 @@ function init() {
         canvas.height = window.innerHeight / 1.1;
         ctx.putImageData(image, 0, 0);
     }
+    function startDraw(ctx, x, y) {
+        ctx.lineWidth = lineWidth;
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
+        ctx.moveTo(x, y);
+        ctx.beginPath();
+    }
+    function draw(ctx, x, y) {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
     resizeCanvas();
-    canvas.addEventListener('mousemove', (e) => {
-        if (canDraw) {
-            const x = e.offsetX;
-            const y = e.offsetY;
-            draw(ctx, x, y);
-        }
+    canvas.addEventListener('click', (e) => {
+        const x = e.offsetX;
+        const y = e.offsetY;
+        ctx.lineTo(x, y);
+        ctx.stroke();
     });
     document.addEventListener('mousedown', (e) => {
         canDraw = true;
@@ -25,17 +36,32 @@ function init() {
         const y = e.offsetY;
         startDraw(ctx, x, y);
     });
-    document.addEventListener('mouseup', () => canDraw = false);
+    canvas.addEventListener('mousemove', (e) => {
+        const x = e.offsetX;
+        const y = e.offsetY;
+        if (canDraw) {
+            draw(ctx, x, y);
+        }
+    });
+    document.addEventListener('mouseup', () => {
+        console.log(ctx);
+        canDraw = false;
+    });
+    document.addEventListener('wheel', (e) => {
+        const step = 3;
+        if (e.deltaY < 0) {
+            lineWidth += step;
+        }
+        else if (lineWidth >= step - 1) {
+            lineWidth -= step;
+        }
+    });
     clearBtn.addEventListener('click', (e) => ctx.clearRect(0, 0, canvas.width, canvas.height));
+    // function animate() {
+    //     requestAnimationFrame(animate);
+    // }
+    // animate();
     // ctx2d.strokeRect(75, 140, 150, 110);
     // ctx2d.fillRect(130, 190, 40, 60);
-}
-function startDraw(ctx, x, y) {
-    ctx.moveTo(x, y);
-    ctx.beginPath();
-}
-function draw(ctx, x, y) {
-    ctx.lineTo(x, y);
-    ctx.stroke();
 }
 init();
--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ function init(): void {
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
     const clearBtn = document.querySelector('.clearBtn') as HTMLButtonElement;
     let canDraw = false;
+    let lineWidth = 1.0;
 
     window.addEventListener('resize', resizeCanvas, false);
 
@@ -12,15 +13,27 @@ function init(): void {
         canvas.height = window.innerHeight / 1.1;
         ctx.putImageData(image, 0, 0);
     }
+
+    function startDraw(ctx: CanvasRenderingContext2D, x: number, y: number): void {
+        ctx.lineWidth = lineWidth;
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
+        ctx.moveTo(x, y);
+        ctx.beginPath();
+    }
+
+    function draw(ctx: CanvasRenderingContext2D, x: number, y: number): void {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+
     resizeCanvas();
 
-
-    canvas.addEventListener('mousemove', (e: MouseEvent) => {
-        if (canDraw) {
-            const x = e.offsetX;
-            const y = e.offsetY;
-            draw(ctx, x, y);
-        }
+    canvas.addEventListener('click', (e: MouseEvent) => {
+        const x = e.offsetX;
+        const y = e.offsetY;
+        ctx.lineTo(x, y);
+        ctx.stroke();
     });
 
     document.addEventListener('mousedown', (e: MouseEvent) => {
@@ -30,23 +43,38 @@ function init(): void {
         startDraw(ctx, x, y);
     });
 
-    document.addEventListener('mouseup', () => canDraw = false);
+    canvas.addEventListener('mousemove', (e: MouseEvent) => {
+        const x = e.offsetX;
+        const y = e.offsetY;
+        if (canDraw) {
+            draw(ctx, x, y);
+        }
+    });
+
+    document.addEventListener('mouseup', () => {
+        console.log(ctx);
+        canDraw = false
+    });
+
+    document.addEventListener('wheel', (e) => {
+        const step = 3;
+        if (e.deltaY < 0) {
+            lineWidth += step;
+        } else if (lineWidth >= step - 1) {
+            lineWidth -= step;
+        }
+    });
 
     clearBtn.addEventListener('click', (e) => ctx.clearRect(0, 0, canvas.width, canvas.height));
 
+    // function animate() {
+
+    //     requestAnimationFrame(animate);
+    // }
+    // animate();
+
     // ctx2d.strokeRect(75, 140, 150, 110);
     // ctx2d.fillRect(130, 190, 40, 60);
-
-}
-
-function startDraw(ctx: CanvasRenderingContext2D, x: number, y: number): void {
-    ctx.moveTo(x, y);
-    ctx.beginPath();
-}
-
-function draw(ctx: CanvasRenderingContext2D, x: number, y: number): void {
-    ctx.lineTo(x, y);
-    ctx.stroke();
 }
 
 init();
