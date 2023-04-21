17ea78afe1a6b819475c94844ae3541b4d852633
Anton Safranovski <kittowned@gmail.com>
2023-04-21 20:01:18 +0300

    added ctrl z

--- main.ts
+++ main.ts
@@ -1,59 +1,71 @@
 function init(): void {
+    /**
+     * Elements
+     */
     const canvas = document.querySelector('#canvas') as HTMLCanvasElement;
-    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    const ctx = canvas.getContext('2d', { willReadFrequently: true }) as CanvasRenderingContext2D;
+    const colorControls = document.querySelector('colors') as HTMLElement;
     const clearBtn = document.querySelector('.clearBtn') as HTMLButtonElement;
-    let canDraw = false;
-    let lineWidth = 1.0;
+    clearBtn.addEventListener('click', () => {
+        clearHistory(history);
+        clearCanvas(ctx, canvas)
+    });
 
-    window.addEventListener('resize', resizeCanvas, false);
+    /**
+     * Initial state
+     */
+    let canDraw: boolean = false;
+    let lineWidth: number = 1.0;
+    let lineCap: CanvasLineCap = "round";
+    let lineJoin: CanvasLineJoin = "round";
+    const history: ImageData[] = [];
+    const colors: string[] = ['red', 'green', 'blue'];
+    const colorElements = colors.map(color => {
+        
+    })
+
+    /** Resize handler */
+    window.addEventListener('resize', resizeCanvas);
 
     function resizeCanvas() {
-        const image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const image = getImageFromCanvas(ctx, canvas);
         canvas.width = window.innerWidth / 1.5;
         canvas.height = window.innerHeight / 1.1;
         ctx.putImageData(image, 0, 0);
     }
 
-    function startDraw(ctx: CanvasRenderingContext2D, x: number, y: number): void {
-        ctx.lineWidth = lineWidth;
-        ctx.lineCap = 'round';
-        ctx.lineJoin = 'round';
-        ctx.moveTo(x, y);
-        ctx.beginPath();
-    }
-
-    function draw(ctx: CanvasRenderingContext2D, x: number, y: number): void {
-        ctx.lineTo(x, y);
-        ctx.stroke();
-    }
-
     resizeCanvas();
-
-    canvas.addEventListener('click', (e: MouseEvent) => {
-        const x = e.offsetX;
-        const y = e.offsetY;
-        ctx.lineTo(x, y);
-        ctx.stroke();
-    });
 
     document.addEventListener('mousedown', (e: MouseEvent) => {
         canDraw = true;
         const x = e.offsetX;
         const y = e.offsetY;
-        startDraw(ctx, x, y);
+        ctx.moveTo(x, y);
+        ctx.beginPath();
     });
 
     canvas.addEventListener('mousemove', (e: MouseEvent) => {
         const x = e.offsetX;
         const y = e.offsetY;
         if (canDraw) {
-            draw(ctx, x, y);
+            const penState: PenState = { lineWidth, lineCap, lineJoin };
+            setPenState(ctx, penState);
+            ctx.lineTo(x, y);
+            ctx.stroke();
         }
     });
 
     document.addEventListener('mouseup', () => {
-        console.log(ctx);
         canDraw = false
+        const newImage = getImageFromCanvas(ctx, canvas);
+        history.push(newImage);
+    });
+
+    canvas.addEventListener('click', (e: MouseEvent) => {
+        const x = e.offsetX;
+        const y = e.offsetY;
+        ctx.lineTo(x, y);
+        ctx.stroke();
     });
 
     document.addEventListener('wheel', (e) => {
@@ -65,16 +77,48 @@ function init(): void {
         }
     });
 
-    clearBtn.addEventListener('click', (e) => ctx.clearRect(0, 0, canvas.width, canvas.height));
+    document.addEventListener('keydown', (e: KeyboardEvent) => {
+        if (e.key === 'z' && e.ctrlKey) {
+            history.pop();
+            const oldImage = history[history.length - 1];
+            clearCanvas(ctx, canvas);
+            if (oldImage) setImageToCanvas(ctx, oldImage);
+        }
+    });
+
 
     // function animate() {
-
     //     requestAnimationFrame(animate);
     // }
     // animate();
+}
 
-    // ctx2d.strokeRect(75, 140, 150, 110);
-    // ctx2d.fillRect(130, 190, 40, 60);
+function getImageFromCanvas(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement): ImageData {
+    return ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+function setImageToCanvas(ctx: CanvasRenderingContext2D, imgData: ImageData): void {
+    ctx.putImageData(imgData, 0, 0);
+}
+
+function clearCanvas(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement): void {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function clearHistory(history: ImageData[]) {
+    history.length = 0;
+}
+
+function setPenState(ctx: CanvasRenderingContext2D, { lineWidth, lineCap, lineJoin }: PenState): void {
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = lineCap;
+    ctx.lineJoin = lineJoin;
+}
+
+interface PenState {
+    lineWidth: number;
+    lineCap: CanvasLineCap;
+    lineJoin: CanvasLineJoin;
 }
 
 init();
